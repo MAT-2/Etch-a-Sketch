96117c07195ac7b51cacd7a1629e9f045bd7103c
sonny <mpcasey@gmail.com>
2023-03-19 13:39:55 -0500

    Made corrections to squares.forEach and the board.querySelectorAll since I mistyped them.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function populateBoard(size){
   let board = document.querySelector(".board");
-  let squares = board.querySelector(".div");
-  squares.foreach((div) => div.remove());
+  let squares = board.querySelectorAll(".div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
   board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
   
