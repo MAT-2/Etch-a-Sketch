15129a390752eb96af227696be228a894b73fd1d
sonny <mpcasey@gmail.com>
2023-03-19 22:03:35 -0500

    Corrected if statement for function changeSize to >=2 && <= 100. Added addEventListener() method to be mouseover including a function to show that whenever the mouse is hovering over thesquares it changes the background color to black. Also changed default grid background color from blue to white.

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function populateBoard(size){
   let amount = size * size;
   for (let i = 0; i < amount; i++) {
     let square = document.createElement('div');
-    square.style.backgroundColor = 'blue';
+    square.addEventListener('mouseover', () => {square.style.backgroundColor = "black"})
+    square.style.backgroundColor = 'white';
     board.insertAdjacentElement('beforeend', square);
   }
 }
@@ -16,7 +17,7 @@ function populateBoard(size){
 populateBoard(16);
 
 function changeSize(input) {
-  if (input <= 2 && input >= 100) {
+  if (input >= 2 && input <= 100) {
     populateBoard(input);
   } else {
     console.log("There are too many squares!");
